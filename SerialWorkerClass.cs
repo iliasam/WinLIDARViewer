6c7640131744983e82455a278fb83d420c631a07
iliasam <iliasam@mail.ru>
2025-10-28 19:28:34 +0300

    Create project.

---
+++ SerialWorkerClass.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Linq;
+using System.Management;
+
+namespace LidarScanningTest1
+{
+	/// <summary>
+	/// Class for working with serial port
+	/// </summary>
+	public class SerialWorkerClass
+	{
+		// Global variable that handle all operations with Serial PORT
+        SerialPort Port;
+        
+        public bool ConnectionState = false;
+        public IniParser SettingsHolder;//Used to store settings
+        public Action<byte[]> DataReceivedCallback;//callback
+		public Action<bool> SerialFailSignal;//callback
+        static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+
+
+        /// <summary>
+        /// Create object for working with the Port
+        /// </summary>
+        /// <param name="ini_file_path"></param> Path to INI-file
+        /// <returns></returns>
+        public SerialWorkerClass(string ini_file_path)
+		{
+            SettingsHolder = new IniParser(ini_file_path);
+
+            myTimer.Tick += new EventHandler(TimerEventProcessor);
+            myTimer.Interval = 30;//50ms
+        }
+
+        private void TimerEventProcessor(object sender, EventArgs e)
+        {
+            try
+            {
+                while (Port.IsOpen && (Port.BytesToRead > 0))
+                {
+                    int bytesToReadCnt = Port.BytesToRead;
+                    //System.Diagnostics.Debug.WriteLine($"Len: {bytesToReadCnt}");
+                    byte[] rxData = new byte[bytesToReadCnt];
+                    Port.Read(rxData, 0, bytesToReadCnt);
+                    DataReceivedCallback?.Invoke(rxData);
+                }
+            }
+            catch (SystemException ex)
+            {
+                MessageBox.Show(ex.Message, "Data Received Event");
+            }
+        }
+
+
+		/// <summary>
+		/// Try to open port
+		/// </summary>
+		/// <param name="port_name"></param> Port name - COM1
+		/// <param name="baud"></param> Baudrate
+		/// <param name="cur_parity"></param> Serial parity
+		/// <param name="cur_stop"></param> Serial stop bits
+		/// <returns></returns>
+		public int OpenSerialPort(string port_name, int baud)
+		{
+        	try
+            {
+                // Create object and connect to selected port
+                Port = new SerialPort(port_name, baud, Parity.None, 8, StopBits.One);
+                Port.NewLine = "\r\n";
+
+                Port.Open();
+				ConnectionState = true;
+
+                if (Port.IsOpen)
+                {
+                    myTimer.Start();
+
+                    SaveSerialSettings();
+                	ConnectionState = true;
+                	return 1;
+                }
+                return -1;
+            }
+        	catch (Exception ex)
+            {
+                // Free resource
+                MessageBox.Show(ex.Message, "Error in open port", MessageBoxButtons.OK);// Show message about error
+                Port.Dispose();
+                ConnectionState = false;
+                myTimer.Stop();
+                return 0;
+            } //end of try
+		}//end of function
+		
+		
+		/// <summary>
+		/// Save Settings
+		/// </summary>
+		/// <returns></returns>
+        public void SaveSerialSettings()
+        {
+        	string serial = Port.PortName;
+        	SettingsHolder.AddSetting("SERIAL_SETTINGS", "serial", serial);
+        	string baud = Port.BaudRate.ToString();
+        	SettingsHolder.AddSetting("SERIAL_SETTINGS", "baud", baud);
+        	string parity = Port.Parity.ToString();
+        	SettingsHolder.AddSetting("SERIAL_SETTINGS", "parity", parity);
+        	string stop_bits = Port.StopBits.ToString();
+        	SettingsHolder.AddSetting("SERIAL_SETTINGS", "stopbits", stop_bits);
+        	SettingsHolder.SaveSettings();
+        }
+        
+        /// <summary>
+        /// Write data to the port
+        /// </summary>
+        /// <returns>Rrturn 1, If writing is possible, or return -1</returns>
+        public int SendDataToSerial(string data)
+        {
+        	if (String.IsNullOrEmpty(data))
+                return -2;
+        	
+        	try
+        	{
+        		if ((Port != null) && (Port.IsOpen))
+        		{
+        			Port.Write(data);
+        			return 1;
+        		}
+        		else
+        		{
+        			ConnectionState = false;
+        			return -1;
+        		}
+        	} catch (Exception e) {
+        		ConnectionState = false;
+        		return -1;
+        		throw;
+        	}
+        }
+        
+        public int SendBinDataToSerial(byte[] data)
+        {
+        	if (data.Length == 0) 
+        		return 1;
+        	
+        	try
+        	{
+        		if ((Port != null) && (Port.IsOpen))
+        		{
+        			Port.Write(data, 0, data.Length);
+        			return 1;
+        		}
+        		else
+        		{
+        			ConnectionState = false;
+        			return -1;
+        		}
+        	} catch (Exception e) {
+        		ConnectionState = false;
+        		return -1;
+        		throw;
+        	}
+        }
+        
+        /// <summary>
+        /// Close port
+        /// </summary>
+        /// <returns></returns>
+        public void ClosePort()
+        {
+        	if (Port != null)
+        	{
+                myTimer.Stop();
+                Port.Close();
+        	}
+        	ConnectionState = false;
+        }
+        
+
+        //*************************************************************************************************************************
+        
+
+        //OLD
+        public List<String> GetSerialPortInfo2()
+        {
+        	List<String > portList = new List<string>();
+        	
+        	using (var searcher = new ManagementObjectSearcher("SELECT * FROM WIN32_SerialPort"))
+        	{
+        		string[] portnames = SerialPort.GetPortNames();
+        		var ports = searcher.Get().Cast<ManagementBaseObject>().ToList();
+        		
+        		var tList = (from n in portnames
+                            join p in ports on n equals p["DeviceID"].ToString()
+                            select n + " - " + p["Caption"]).ToList();
+        		
+        		foreach (string s in tList)
+                {
+        			portList.Add(s);
+                }
+
+        	}
+        	return portList;
+        }
+
+        //You need to add System.Management.dll to your project references.
+        internal class ProcessConnection
+        {
+
+        	public static ConnectionOptions ProcessConnectionOptions()
+        	{
+        		ConnectionOptions options = new ConnectionOptions();
+        		options.Impersonation = ImpersonationLevel.Impersonate;
+        		options.Authentication = AuthenticationLevel.Default;
+        		options.EnablePrivileges = true;
+        		return options;
+        	}
+        	
+        	public static ManagementScope ConnectionScope(string machineName, ConnectionOptions options, string path)
+        	{
+        		ManagementScope connectScope = new ManagementScope();
+        		connectScope.Path = new ManagementPath(@"\\" + machineName + path);
+        		connectScope.Options = options;
+        		connectScope.Connect();
+        		return connectScope;
+        	}
+        }
+        
+        
+        public List<String> GetSerialPortInfo()
+        {
+        	List<String> comPortInfoList = new List<String>();
+        	
+        	ConnectionOptions options = ProcessConnection.ProcessConnectionOptions();
+        	ManagementScope connectionScope = ProcessConnection.ConnectionScope(Environment.MachineName, options, @"\root\CIMV2");
+        	
+        	ObjectQuery objectQuery = new ObjectQuery("SELECT * FROM Win32_PnPEntity WHERE ConfigManagerErrorCode = 0");
+        	ManagementObjectSearcher comPortSearcher = new ManagementObjectSearcher(connectionScope, objectQuery);
+        	
+        	using (comPortSearcher)
+        	{
+        		string caption = null;
+        		foreach (ManagementObject obj in comPortSearcher.Get())
+        		{
+        			if (obj != null)
+        			{
+        				object captionObj = obj["Caption"];
+        				if (captionObj != null)
+        				{
+        					caption = captionObj.ToString();
+        					if (caption.Contains("(COM"))
+        					{
+        						String comPortInfo = "";
+        						comPortInfo = caption.Substring(caption.LastIndexOf("(COM")).Replace("(", string.Empty).Replace(")", string.Empty);
+        						comPortInfo = comPortInfo + " " + caption;
+        						comPortInfoList.Add(comPortInfo);
+        					}
+        				}
+        			}
+        		}
+        	}
+        	return comPortInfoList;
+        }
+        
+        
+        
+        //"COM1 - xxxxxxx" -> "COM1"
+        public String GetSerialName(String info_str)
+        {
+        	String result = "";
+        	int pos = info_str.IndexOf(' ');
+        	if (pos < 0) return info_str;
+        	result = info_str.Substring(0,pos);
+        	return result;
+        }
+        
+        
+	}//end of CLASS
+}
